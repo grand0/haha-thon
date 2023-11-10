fe6227955f055eb33fda51c9df80f88914a24b06
infimum05772 <r.arifulina@gmail.com>
2023-11-10 15:19:15 +0300

    inserted values into Measurement int constraints

---
+++ scripts/V1.22__insertValuesIntoMeasurementsIntConstraints.sql
@@ -0,0 +1,3 @@
+insert into Measurement_int_constraints(measurement_name_id, min, max) values (1, 0, 16777215);
+insert into Measurement_int_constraints(measurement_name_id, min, max) values (2, 0, 100);
+insert into Measurement_int_constraints(measurement_name_id, min, max) values (3, 0, 1);
