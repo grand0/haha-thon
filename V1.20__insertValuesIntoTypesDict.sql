b98b616ca2cf531cf99f7d304b3d0a80ee641a47
infimum05772 <r.arifulina@gmail.com>
2023-11-10 14:57:23 +0300

    values inserted into dicts Types and Measurement_names

---
+++ V1.20__insertValuesIntoTypesDict.sql
@@ -0,0 +1,6 @@
+insert into Types(type) values ('Moving sensor');
+insert into Types(type) values ('Water Leakage Sensor');
+insert into Types(type) values ('Window Blind Sensor');
+insert into Types(type) values ('Smoke Sensor');
+insert into Types(type) values ('Lighting Sensor');
+insert into Types(type) values ('Door And Window Sensor');
---
+++ V1.21__insertValuesIntoMeasurementNamesDict.sql
@@ -0,0 +1,3 @@
+insert into Measurement_names(name) values ('color');
+insert into Measurement_names(name) values ('intensity');
+insert into Measurement_names(name) values ('detected');
